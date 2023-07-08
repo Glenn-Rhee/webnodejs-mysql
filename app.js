69aa7948db2d75d89ff097e4526f4fc27d8d2bab
Glenn-Rhee <glennviktor5@gmail.com>
2023-07-08 14:26:58 +0700

    Inisiasi repo and made navbar

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+const port = 3000;
+
+// Setup ejs
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// 
+app.get("/", (req,res)=>{
+    res.render("Home", {
+        layout:"layouts/main-layout",
+        title: "Glenn Rhee's"
+    })
+})
+
+
+app.use((req,res,next)=>{
+    res.status(404).send("Halaman tidak ditemukan");
+    next();
+})
+
+
+app.listen(port, () => {
+    console.log("Web Server listening on port " + port);
+})
