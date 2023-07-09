44c97e7aef13eee2cc26c7bc2dada9d8c2cabca1
Glenn-Rhee <glennviktor5@gmail.com>
2023-07-09 13:53:58 +0700

    Menambahkan fitur active page

--- public/js/animate.js
+++ public/js/animate.js
@@ -1,8 +1,8 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
-menuToggle.addEventListener("click", function(){
+menuToggle.addEventListener("click", function () {
     menuToggle.classList.toggle("trigger");
-    if(!menu.classList.contains("slider")){
+    if (!menu.classList.contains("slider")) {
         menu.classList.toggle("slider");
     } else {
         menu.classList.toggle("closeSlider");
@@ -13,3 +13,16 @@ menuToggle.addEventListener("click", function(){
     }
 })
 
+const menuPage = document.querySelectorAll(".menu-page");
+const url = window.location.href;
+let activeUrl = url.split("/");
+activeUrl = activeUrl[activeUrl.length - 1];
+if (activeUrl == "about") {
+    menuPage[0].classList.add("active");
+} else if (activeUrl == "services") {
+    menuPage[1].classList.add("active");
+} else if (activeUrl == "portfolio") {
+    menuPage[2].classList.add("active");
+} else if (activeUrl == "contact") {
+    menuPage[3].classList.add("active");
+}
