8a5b3923429353bfeb4ad5f326323ba40f6be26e
Glenn-Rhee <glennviktor5@gmail.com>
2023-07-09 20:56:34 +0700

    Menambahkan fitur portfolio di semua device

--- app.js
+++ app.js
@@ -11,30 +11,38 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 // 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.render("home", {
-        layout:"layouts/main-layout",
+        layout: "layouts/main-layout",
         title: "Glenn Rhee's"
     })
 })
 
 // Home
-app.get("/about", (req,res)=>{
+app.get("/about", (req, res) => {
     res.render("about", {
-        layout:"layouts/main-layout",
-        title:"Glenn Rhee's | About"
+        layout: "layouts/main-layout",
+        title: "Glenn Rhee's | About"
     })
 })
 
 // Services
-app.get("/services", (req,res)=>{
+app.get("/services", (req, res) => {
     res.render("services", {
         layout: "layouts/main-layout",
         title: "Glenn Rhee's | Services"
     })
 })
 
-app.use((req,res,next)=>{
+// Portfolio
+app.get("/portfolio", (req, res) => {
+    res.render("portfolio", {
+        layout: "layouts/main-layout",
+        title: "Glenn Rhee's | Portfolio"
+    })
+})
+
+app.use((req, res, next) => {
     res.status(404).send("Halaman tidak ditemukan");
     next();
 })
