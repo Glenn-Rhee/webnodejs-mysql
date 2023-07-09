2366593e37425a69a723f5a69f4132aec98e6444
Glenn-Rhee <glennviktor5@gmail.com>
2023-07-10 01:02:40 +0700

    Finished

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
+const { check, validationResult, body } = require("express-validator");
+const { addData, connection } = require("./data/db");
 const app = express();
 const port = 3000;
 
@@ -10,7 +12,7 @@ app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// 
+// Home
 app.get("/", (req, res) => {
     res.render("home", {
         layout: "layouts/main-layout",
@@ -18,7 +20,7 @@ app.get("/", (req, res) => {
     })
 })
 
-// Home
+// About
 app.get("/about", (req, res) => {
     res.render("about", {
         layout: "layouts/main-layout",
@@ -50,11 +52,46 @@ app.get("/contact", (req, res) => {
     })
 })
 
+// Melakukan sesuatu ke contact 
+app.post("/contact", [
+    check("phoneNumber", "Masukkan format no telpon yang benar").isMobilePhone("id-ID"),
+    check("email", "Masukkan format email yang benar").isEmail()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const dataError = errors.array();
+        res.render("contact", {
+            layout: "layouts/main-layout",
+            title: "Glenn Rhee's | Contact Us",
+            dataError
+        })
+    } else {
+        try {
+            const data = req.body;
+            await addData(data);
+            res.render("contact", {
+                layout: "layouts/main-layout",
+                title: "Glenn Rhee's | Contact Us",
+                message: "Thank You for your messages :)"    
+            })
+        } catch(err){
+            console.log(err);
+        } finally {
+            connection.end((err)=>{
+                if(err) {
+                    console.log("Gagal ditutup ", err);
+                } else {
+                    console.log("Berhaisl tutup");
+                }
+            });
+        }
+    }
+})
+
 app.use((req, res, next) => {
     res.status(404).send("Halaman tidak ditemukan");
     next();
 })
-
 
 app.listen(port, () => {
     console.log("Web Server listening on port " + port);
---
+++ data/db.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'contact'
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.log("Gagal connect");
+    } else {
+        console.log("Berhasil terhubung");
+    }
+});
+
+const addData = async (data) => {
+    return new Promise((resolve, reject) => {
+        connection.query("INSERT INTO `gleen_rhee's` SET ?", data, (err, res) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(res)
+            }
+        })
+    })
+}
+
+module.exports = { addData, connection }
